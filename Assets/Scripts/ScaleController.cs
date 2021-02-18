1014489c4a61ac1cb6dfd0ce419f0f865c181d5d
Aakash Makhija <43120661+aakash-m@users.noreply.github.com>
2021-02-18 14:29:14 +0100

    Prototype of AR game

---
+++ Assets/Scripts/ScaleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+public class ScaleController : MonoBehaviour
+{
+    ARSessionOrigin m_ARSessionOrigin;
+
+    public Slider scale_Slider;
+
+    private void Awake()
+    {
+        m_ARSessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale_Slider.onValueChanged.AddListener(OnSlidervalueChanged);
+    }
+
+    public void OnSlidervalueChanged(float value)
+    {
+        if (scale_Slider != null)
+        {
+            m_ARSessionOrigin.transform.localScale = Vector3.one / value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class SpawnManager : MonoBehaviourPunCallbacks
+{
+
+    public GameObject[] playerPrefabs;
+    public Transform[] spawnPositions;
+
+    public GameObject battleArenaGameobject;
+
+    public enum RaiseEventCodes
+    {
+        PlayerSpawnEventCode = 0
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += onEvent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived -= onEvent;
+    }
+
+
+    #region Photon Callback Methods
+
+    void onEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == (byte)RaiseEventCodes.PlayerSpawnEventCode)
+        {
+            //Order should be as written for object[] data written for sending from line 99
+            object[] data = (object[])photonEvent.CustomData;
+            Vector3 receivedPosition = (Vector3)data[0];
+            Quaternion receivedRotation = (Quaternion)data[1];
+            int receivedPlayerSelectionData = (int)data[3];
+
+            GameObject player = Instantiate(playerPrefabs[receivedPlayerSelectionData], receivedPosition + battleArenaGameobject.transform.position, receivedRotation);
+            PhotonView _photonView = GetComponent<PhotonView>();
+            _photonView.ViewID = (int)data[2];
+
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            /*
+            object playerSelectionNumber;
+            if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARSpinnerTopGame.PLAYER_SELECTION_NUMBER, out playerSelectionNumber))
+            {
+                Debug.Log("Player selection number is "+ (int)playerSelectionNumber);
+
+                int randomSpawnPoint = Random.Range(0, spawnPositions.Length-1);
+                Vector3 instantiatePosition = spawnPositions[randomSpawnPoint].position;
+
+
+                PhotonNetwork.Instantiate(playerPrefabs[(int)playerSelectionNumber].name, instantiatePosition, Quaternion.identity);
+
+            }*/
+            SpawnPlayer();
+
+
+        }
+
+
+       
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void SpawnPlayer()
+    {
+        object playerSelectionNumber;
+        if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARSpinnerTopGame.PLAYER_SELECTION_NUMBER, out playerSelectionNumber))
+        {
+            Debug.Log("Player selection number is " + (int)playerSelectionNumber);
+
+            int randomSpawnPoint = Random.Range(0, spawnPositions.Length - 1);
+            Vector3 instantiatePosition = spawnPositions[randomSpawnPoint].position;
+
+            GameObject playerGameobject = Instantiate(playerPrefabs[(int)playerSelectionNumber], instantiatePosition, Quaternion.identity);
+
+            PhotonView _photonView = playerGameobject.GetComponent<PhotonView>();
+
+            if (PhotonNetwork.AllocateViewID(_photonView))
+            {
+                object[] data = new object[]
+                {
+                    
+                    playerGameobject.transform.position - battleArenaGameobject.transform.position, //Extracting the position of arena for us
+                    playerGameobject.transform.rotation,
+                    _photonView.ViewID,
+                    playerSelectionNumber
+                };
+
+                RaiseEventOptions raiseEventOptions = new RaiseEventOptions
+                {
+                    Receivers = ReceiverGroup.Others, //Event will be sent to other players except me because for me it will instantiate player twice
+                    CachingOption = EventCaching.AddToRoomCache
+                };
+
+                SendOptions sendOptions = new SendOptions
+                {
+                    Reliability = true,
+                };
+
+                //Raise Events
+                PhotonNetwork.RaiseEvent((byte)RaiseEventCodes.PlayerSpawnEventCode, data, raiseEventOptions, sendOptions);
+
+            }
+            else
+            {
+                Destroy(playerGameobject);
+                Debug.Log("Failed to allocate viewID");
+            }
+        }
+    }
+
+    #endregion
+
+}
